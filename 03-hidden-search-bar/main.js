55bea8ecf59f7a1e0cdc9231b7e3467c7f28d559
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-04-22 17:32:32 +0530

    Added fading heading

--- 03-hidden-search-bar/main.js
+++ 03-hidden-search-bar/main.js
@@ -1,8 +1,23 @@
+const promisifiedTimeout = (ms) => new Promise((res) => setTimeout(res, ms));
+
 const container = document.querySelector(".container");
 const icon = document.querySelector(".icon");
 const searchBar = document.querySelector(".search");
+const headingElement = document.querySelector(".heading");
 
 icon.addEventListener("click", () => {
   container.classList.toggle("onClick");
   searchBar.focus();
 });
+
+const headingText = "idden Search Widget";
+
+const attachLetter = async () => {
+  for (let i = 0; i < headingText.length; i++) {
+    await promisifiedTimeout(200);
+    const letter = headingText[i];
+    headingElement.innerHTML += letter;
+  }
+};
+
+attachLetter();
